a5635a10020dd54814ab6698700718434e18ff41
Cerkute <kristijonas.savanevicius@vka.lt>
2016-07-17 07:20:24 +0300

    Rebased

--- www/services/Resources.js
+++ www/services/Resources.js
@@ -11,25 +11,27 @@
             checkUser: checkUser,
             setMerchantUserSession: setMerchantUserSession,
             getMerchantUserSession: getMerchantUserSession,
+            destroyMerchantUserSession: destroyMerchantUserSession,
             setClientUserSession: setClientUserSession,
             getClientUserSession: getClientUserSession,
+            destroyClientUserSession: destroyClientUserSession,
             setUserData: setUserData,
             getUserData: getUserData
         };
         return resources;
 
-        var userData = null,
-            merchantUserSession = null,
-            clientUserSession = null;
+        var userData = null;
 
         function setUserData(val) {userData = val;}
         function getUserData() {return userData;}
 
-        function setMerchantUserSession(val) {merchantUserSession = val;}
-        function getMerchantUserSession() {return merchantUserSession;}
+        function setMerchantUserSession(val) {$cookies.put('merchant', val);}
+        function getMerchantUserSession() {return $cookies.get('merchant');}
+        function destroyMerchantUserSession() {$cookies.remove('merchant');}
 
-        function setClientUserSession(val) {clientUserSession = val;}
-        function getClientUserSession() {return clientUserSession;}
+        function setClientUserSession(val) {$cookies.put('client', val);}
+        function getClientUserSession() {return $cookies.get('client');}
+        function destroyClientUserSession() {$cookies.remove('client');}
 
         function checkUser() {
 
